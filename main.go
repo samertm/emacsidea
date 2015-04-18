5cd304e68cd845305ecbcf19061e1fa3576de686
Samer Masterson <samer@samertm.com>
2015-04-18 03:50:05 -0700

    display .emacs file

--- main.go
+++ main.go
@@ -1,9 +1,15 @@
 package main
 
 import (
+	"fmt"
 	"io"
+	"io/ioutil"
 	"log"
 	"net/http"
+	"net/url"
+	"os"
+	"os/exec"
+	"path/filepath"
 
 	"github.com/google/go-github/github"
 	"github.com/gorilla/mux"
@@ -22,16 +28,65 @@ func serveProfile(w http.ResponseWriter, r *http.Request) error {
 	if err != nil {
 		return err
 	}
-	var repos []github.Repository
+	var found bool
+	var emacsConfigRepo github.Repository
 	for _, r := range rs {
-		if r.Language != nil && *r.Language == "Emacs Lisp" {
-			repos = append(repos, r)
+		if *r.Name == ".emacs.d" {
+			emacsConfigRepo = r
+			found = true
+			break
 		}
 	}
-	for _, r := range repos {
-		io.WriteString(w, "<p>Elisp repo: "+*r.Name+"</p>")
+	if !found {
+		io.WriteString(w, "<p>Could not find .emacs.d repo</p>")
+		return nil
 	}
+	code, err := getCode(*emacsConfigRepo.CloneURL)
+	if err != nil {
+		return err
+	}
+	io.WriteString(w, "<pre>")
+	io.WriteString(w, code)
+	io.WriteString(w, "</pre>")
 	return nil
+}
+
+var storeDir = filepath.Join(os.TempDir(), "emacs-code-store")
+
+// just get init.el for now.
+func getCode(cloneURL string) (string, error) {
+	u, err := url.Parse(cloneURL)
+	if err != nil {
+		return "", err
+	}
+	p := filepath.Join(storeDir, u.Host, u.Path)
+	if _, err := os.Stat(p); err != nil {
+		// Repo doesn't exist, create it.
+		if err := os.MkdirAll(p, os.ModePerm); err != nil {
+			return "", err
+		}
+		c := exec.Command("git", "init")
+		c.Dir = p
+		if o, err := c.CombinedOutput(); err != nil {
+			return "", fmt.Errorf("%s, err: %s", o, err)
+		}
+		c = exec.Command("git", "remote", "add", "origin", cloneURL)
+		c.Dir = p
+
+		if o, err := c.CombinedOutput(); err != nil {
+			return "", fmt.Errorf("%s, err: %s", o, err)
+		}
+	}
+	c := exec.Command("git", "pull", "origin", "master")
+	c.Dir = p
+	if o, err := c.CombinedOutput(); err != nil {
+		return "", fmt.Errorf("%s, err: %s", o, err)
+	}
+	f, err := ioutil.ReadFile(filepath.Join(p, "init.el"))
+	if err != nil {
+		return "", err
+	}
+	return string(f), nil
 }
 
 type handler func(http.ResponseWriter, *http.Request) error
